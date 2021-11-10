9dc1dd70167211a6aecbe9c09536116a71bea6b9
John Leng <johnmarkleng@gmail.com>
2021-11-10 18:31:55 -0500

    UI updates

--- src/application/CurrentOrderController.java
+++ src/application/CurrentOrderController.java
@@ -1,4 +1,42 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class CurrentOrderController {
+
+    @FXML
+    private TextField customerNumber;
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private TextField orderTotal;
+
+    @FXML
+    private Button placeOrderButton;
+
+    @FXML
+    private Button removePizzaButton;
+
+    @FXML
+    private TextField salesTaxField;
+
+    @FXML
+    private TextField subtotalField;
+
+    @FXML
+    void placeOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removePizza(ActionEvent event) {
+
+    }
+
 }
--- src/application/StoreOrderController.java
+++ src/application/StoreOrderController.java
@@ -1,4 +1,37 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
 public class StoreOrderController {
+
+    @FXML
+    private Button cancelStoreOrderButton;
+
+    @FXML
+    private TextField entireOrderTotal;
+
+    @FXML
+    private Button exportStoreOrdersButton;
+
+    @FXML
+    private MenuButton menuButton;
+
+    @FXML
+    private ListView<?> storeOrderList;
+
+    @FXML
+    void cancelStoreOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exportStoreOrders(ActionEvent event) {
+
+    }
+
 }
