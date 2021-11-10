816e358a1cf258778ca1298f1ed4e5c943838d20
jasonz-li <jasonli.0820@yahoo.com>
2021-11-10 17:23:19 -0500

    images

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainMenuView.fxml"));
-        primaryStage.setTitle("Main Menu");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("RU Pizzeria Home");
+        primaryStage.setScene(new Scene(root, 818, 692));
         primaryStage.show();
     }
 
